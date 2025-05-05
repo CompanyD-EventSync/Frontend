674bb9f9e542ebdb66a361e9444f3acc501b9b09
Mathib417 <mathib0417@gmail.com>
2025-05-05 20:04:06 +0530

    react updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <h2>Welcome to EventSync 🎉</h2>;
+  }
+  export default HomePage;
+  
