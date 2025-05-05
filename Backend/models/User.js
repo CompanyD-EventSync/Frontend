5c23b50795e74da5b93d215c8abd554ccb85a7e2
Mathib417 <mathib0417@gmail.com>
2025-05-05 18:13:47 +0530

    update models/User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,3 +1,4 @@
+/*
 // models/User.js (Sequelize version)
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
@@ -7,5 +8,16 @@ const User = sequelize.define("User", {
   email: { type: DataTypes.STRING, unique: true },
   password: DataTypes.STRING,
   role: { type: DataTypes.ENUM("USER", "ADMIN"), defaultValue: "USER" }
+});
+module.exports = User;
+*/
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    email: { type: DataTypes.STRING, unique: true },
+    password: DataTypes.STRING,
+    role: { type: DataTypes.ENUM("USER", "ADMIN"), defaultValue: "USER" }
 });
 module.exports = User;
